17276493b8f7280ce9724ebdc2515faf739c47ba
seosky123 <76084258+seosky123@users.noreply.github.com>
2021-05-22 23:28:26 +0900

    Add files via upload

--- frontend/JDN WEBSITE-revise/js/header.js
+++ frontend/JDN WEBSITE-revise/js/header.js
@@ -34,22 +34,42 @@ window.addEventListener('scroll',reveal);
 
 
 //모바일 네비게이션//
+
+let mediaQuery = window.matchMedia('(max-width: 1200px)');
+if (mediaQuery.matches) {
+
+
 const burger = document.querySelector(".burger");
+const bar = document.querySelectorAll(".Bbar");
 const mobileNav = document.querySelector(".firstLinks");
 function openNav(){
-mobileNav.classList.toggle('navHide');
+mobileNav.classList.toggle('nav-active');
+for (let i = 0; i < bar.length; i++) {
+    bar[i].classList.toggle('clicked');
 }
+    
+}
+console.log(bar);
+burger.addEventListener("click", openNav);
 
-     burger.addEventListener("click", openNav);
-
+    
+    
 function openList(event){
 const mainList = event.target.parentElement;
+
+if(mainList.classList.contains('onclick')){
 for (let i = 0; i < firstLinks.length; i++) {
     firstLinks[i].classList.remove("onclick");
-}
+}} else
+    {
+    for (let i = 0; i < firstLinks.length; i++) {
+    firstLinks[i].classList.remove("onclick");
+} 
     mainList.classList.add("onclick");
-}
+}};
 
 for (let i = 0; i < firstLinks.length; i++) {
     firstLinks[i].addEventListener('click',openList);
+}  
+    
 }
