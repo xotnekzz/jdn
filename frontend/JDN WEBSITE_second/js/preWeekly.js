5eae5f89eea07cfd04e1212e885781af549c3879
seosky123 <76084258+seosky123@users.noreply.github.com>
2021-06-11 20:24:44 +0900

    Add files via upload

---
+++ frontend/JDN WEBSITE_second/js/preWeekly.js
@@ -0,0 +1,32 @@
+const list = document.getElementsByClassName("first-list");
+
+
+for(i=0; i<list.length;i++){
+list[i].addEventListener("click",function(event){
+if(event.target.parentNode.classList.contains('clicked')){
+event.target.parentNode.classList.remove("clicked");
+}
+    else{
+    for(i=0; i<list.length;i++){
+list[i].classList.remove("clicked");
+}
+event.target.parentNode.classList.toggle("clicked");
+};
+
+})
+}
+
+
+
+
+if(screen.width>=1200){
+document.write(`<script src='../js/weekly.js'></script>`);
+}else{
+window.onload = function () {
+    $('.target')
+        .append('<div class="photo"></div>')
+        .children('.photo').css({
+            'background-image': 'url(' + $('.target').attr('data-image') + ')'
+        })
+    };
+}
