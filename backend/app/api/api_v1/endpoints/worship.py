e2e532172e319180d3223ede1cb8261a8c34ac0f
tskim <tskim@tskimui-MacBookPro.local>
2021-06-20 22:16:04 +0900

    test orm

--- backend/app/api/api_v1/endpoints/worship.py
+++ backend/app/api/api_v1/endpoints/worship.py
@@ -1,8 +1,25 @@
-from fastapi import APIRouter
+from typing import List
+from fastapi import APIRouter, Depends
 from fastapi.responses import PlainTextResponse
+from sqlalchemy.orm import Session
+
+
+from app.orm import models, schemas
+from app.orm.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+# Dependency
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 router = APIRouter()
 
-@router.get("/", response_class=PlainTextResponse)
-async def read():
-    return "TEST"
+@router.get("/", response_model=List[schemas.Worship])
+def read_worships(db: Session = Depends(get_db)):
+    worships = db.query(models.Worship).all()
+    return worships
