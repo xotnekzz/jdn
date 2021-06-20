e4072d89822d61d24c4ccba6bc26b7ae23e05e08
tskim <tskim@tskimui-MacBookPro.local>
2021-06-20 20:23:26 +0900

    orm test

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import worship
+from app.api.api_v1.endpoints import worship, test
 
 api_router = APIRouter()
 api_router.include_router(worship.router, prefix="/api/v1", tags=["worship"])
+api_router.include_router(test.router, prefix="/api/v1", tags=["test"])
