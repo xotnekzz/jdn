204c79f857bfaf9da043825c61a880917b753ae8
tskim <tskim@tskimui-MacBookPro.local>
2021-06-20 15:53:10 +0900

    update

--- backend/start.sh
+++ backend/start.sh
@@ -5,7 +5,7 @@ cd /var/www/jdn
 git checkout -- .
 git pull
 
-pip install -r /var/www/jdn/config/requirements.txt
+pip install -r /var/www/jdn/backendconfig/requirements.txt
 
 cd backend
 uvicorn app.main:app --host 0.0.0.0 --reload --port 80
