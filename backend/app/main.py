8ce0f0ce09752e8e0d38f11319520d811827d235
tskim <tskim@tskimui-MacBookPro.local>
2021-06-20 22:20:54 +0900

    main.py:

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from app.api.api_v1.api import api_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
 app.include_router(api_router)
 
 @app.get("/")
