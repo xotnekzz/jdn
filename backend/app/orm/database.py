f34ca115d6618bebd3ff11d1d3c90c02689589f2
tskim <tskim@tskimui-MacBookPro.local>
2021-06-28 22:22:46 +0900

    orm setting fix

--- backend/app/orm/database.py
+++ backend/app/orm/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:jdn1!@127.0.0.1:3306/jdn_web"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
