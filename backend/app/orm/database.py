a827cc6c41ae43a249d7d890bdaca46ca1739539
tskim <tskim@tskimui-MacBookPro.local>
2021-06-28 22:28:20 +0900

    fix

--- backend/app/orm/database.py
+++ backend/app/orm/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:jdn1!@127.0.0.1:3306/jdn_web"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:jdn1!@jdn-db:3306/jdn_web"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
